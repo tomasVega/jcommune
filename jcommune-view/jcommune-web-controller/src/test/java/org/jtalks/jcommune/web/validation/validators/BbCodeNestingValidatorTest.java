9c804e8fe241f1ab87f9097aa2bc365fa9701455
Temdegon <willcode4food@tut.by>
2013-12-24 18:37:42 +0300

    JC-1688: changed validation to kefir-bb

--- jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/validators/BbCodeNestingValidatorTest.java
+++ jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/validators/BbCodeNestingValidatorTest.java
@@ -22,6 +22,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.apache.commons.lang.StringUtils.repeat;
+import org.junit.Assert;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
@@ -39,31 +40,69 @@ public class BbCodeNestingValidatorTest {
     public void initMocks() throws NoSuchFieldException {
         MockitoAnnotations.initMocks(this);
         
-        when(annotation.maxNestingValue()).thenReturn(50);
+        when(annotation.maxNestingValue()).thenReturn(2);
         when(userService.getCurrentUser()).thenReturn(new JCUser("", "", ""));
         
         bbCodeNestingValidator = new BbCodeNestingValidator(userService);
         bbCodeNestingValidator.initialize(annotation);
     }
 
-    @Test(dataProvider = "tooDeepNestingMessages")
-    public void testValidationFail(String message) {
-        assertFalse(bbCodeNestingValidator.isValid(message, null));
-    }
-
-    @Test(dataProvider = "validMessages")
-    public void testValidationSuccess(String message) {
-        assertTrue(bbCodeNestingValidator.isValid(message, null));
-    }
-
     @Test
-    public void onlyOpeningBbCodesShouldResultInError() {
-        assertFalse(bbCodeNestingValidator.isValid(repeat("[b]", 100), null));
+    public void testTooDeepNestingWithListItems() {
+        assertFalse(bbCodeNestingValidator.isValid("[list][*]zzz[list][*]zzz[/*][/list][/*][/list]", null),
+                    "Nesting limit is 2, but actual nesting is 4");        
+    }
+    
+    @Test
+    public void testListItemsNesting() {
+        assertFalse(bbCodeNestingValidator.isValid("[list][*]zzz[list][*]zzz[/*][/list][/*][/list]", null),
+                    "Nesting limit is 2, but actual nesting is 4");
+        assertTrue(bbCodeNestingValidator.isValid("[*][*][*][*][*]", null), "limit doesn't affect [*]");
+    }
+    
+    @Test
+    public void testNestingLimit() {
+        assertInvalid("[b][b][b]text", "The limit is 2, actual nesting is 3");
+        assertInvalid("[b][b][b]text[/b][/b][/b]", "The limit is 2, actual nesting is 3");
+        assertValid("[b][b]text[/b][/b]", "The limit is 2, actual nesting is 2");
+        assertValid("[/b][/b][/b][/b][/b][/b]", "The limit doesn't affect closing tags without respective open tags");
+        assertValid("[b]1[/b]2[b]3[/b]4[b]5[/b]6[b]7[/b]8[b]9[/b]", "The limit is 2, actual nesting is 1");
+    }
+    
+    @Test
+    public void testQuotes() {
+        assertInvalid("[quote][b][b]text[/b][/b][/quote]", "The limit is 2, actual nesting is 3");
+        assertValid("[quote][b]text[/b][/quote]", "The limit is 2, actual nesting is 2");
+    }
+    
+    @Test
+    public void testNamedQuotes() {
+        assertValid("[quote=\"name\"][b]text[/b][/qoute]");
+        assertInvalid("[quote=\"name\"][b][b]text[/b][/b][/qoute]");
     }
 
-    @Test
-    public void onlyClosingBbCodesShouldResultInError() {
-        assertFalse(bbCodeNestingValidator.isValid(repeat("[/b]", 100), null));
+    void assertValid(String toValidate) {
+        assertValid(toValidate, null);
+    }
+    
+    public void assertValid(String toValidate, String message) {
+        if (!bbCodeNestingValidator.isValid(toValidate, null)) {
+            if (message == null) {
+                Assert.fail(toValidate + ": expected <valid>, but was <invalid>");
+            } else {
+                Assert.fail(toValidate + " : " + message + ": expected <valid>, but was <invalid>");
+            }
+        }
+    }
+    
+    public void assertInvalid(String toValidate, String message) {
+        if (bbCodeNestingValidator.isValid(toValidate, null)) {
+            if (message == null) {
+                Assert.fail(toValidate + ": expected <invalid>, but was <valid>");
+            } else {
+                Assert.fail(toValidate + " : "  + message + ": expected <invalid>, but was <valid>");
+            }
+        }
     }
 
     @Test
@@ -71,7 +110,7 @@ public class BbCodeNestingValidatorTest {
         assertTrue(bbCodeNestingValidator.isValid(null, null));
         assertTrue(bbCodeNestingValidator.isValid("", null));
     }
-
+    
     @DataProvider
     public String[][] validMessages() {
         return new String[][]{ // {"message"}
@@ -106,5 +145,9 @@ public class BbCodeNestingValidatorTest {
                 {repeat("[quote=\"\"]",51)},
                 {"[quote=\""+repeat("[url=123]",51)+"\"]"}
         };
+    }
+
+    private void assertInvalid(String toValidate) {
+        assertInvalid(toValidate, null);
     }
 }
